5ecd658a672713b2716443fc6ea3168b15c321d9
philippbruhin <info@philippbruhin.ch>
2017-08-23 23:29:52 +0200

    Added REpository Model

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
 namespace PartyInvites.Controllers
+
 {
     public class HomeController : Controller
     {
@@ -9,6 +12,19 @@ namespace PartyInvites.Controllers
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: store repsonse from guest
+            return View();
         }
     }
 }
